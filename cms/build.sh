869a18658ca0ce163cf989f7600e570dde65ed88
Mathias Piletti <mathiasp50@gmail.com>
2021-02-23 16:23:12 +0100

    Build5

--- cms/build.sh
+++ cms/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Move the Platform.sh-specific configuration.
-rm ./cms/config/database.json && mv platformsh/database.js config/environments/development/database.js
-rm ./cms/config/server.json && mv platformsh/server.json config/environments/development/server.json
+rm config/database.json && mv platformsh/database.js config/environments/development/database.js
+rm config/server.json && mv platformsh/server.json config/environments/development/server.json
 
 # Rebuild the admin panel.
 yarn build
 
 # Make start command executable.
-chmod +x ./cms/start.sh
+chmod +x start.sh
