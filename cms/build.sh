c8f24afda3d8fa1f3389a63bca65190b1c475962
Mathias Piletti <mathiasp50@gmail.com>
2021-02-23 16:17:22 +0100

    Build3

--- cms/build.sh
+++ cms/build.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# Create quickstart Strapi project.
-yarn install
-yarn add pg
-yarn add platformsh-config
-
 # Move the Platform.sh-specific configuration.
-rm config/environments/development/database.json && mv platformsh/database.js config/environments/development/database.js
-rm config/environments/development/server.json && mv platformsh/server.json config/environments/development/server.json
+rm config/database.json && mv platformsh/database.js config/environments/development/database.js
+rm config/server.json && mv platformsh/server.json config/environments/development/server.json
 
 # Rebuild the admin panel.
-yarn strapi install graphql
 yarn build
 
 # Make start command executable.
