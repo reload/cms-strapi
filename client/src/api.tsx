82d151e0c78ea29bf5c6da98bec8b2208f98cec7
Mathias Piletti <mathiasp50@gmail.com>
2021-02-26 11:44:10 +0100

    Adjusted client platform.app.yaml 5

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -2,7 +2,7 @@ export function getApiUrl() {
   if (process.env.NODE_ENV === "development") {
     return `http://localhost:1337`;
   } else {
-    return `https://api.${window.location.host}`;
+    return `https://api.${window.location.host.replace("www.", "")}`;
   }
 }
 
@@ -10,6 +10,6 @@ export function getApiToken() {
   if (process.env.NODE_ENV === "development") {
     return `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEyMjc4ODI2LCJleHAiOjE2MTQ4NzA4MjZ9.WMPFKUGYMR6QER-voz7WG1sAs-t8yO-09WQtLwJAQY0`;
   } else {
-    return `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjEyMjc4ODI2LCJleHAiOjE2MTQ4NzA4MjZ9.WMPFKUGYMR6QER-voz7WG1sAs-t8yO-09WQtLwJAQY0`;
+    return `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjE0MzM2MDcyLCJleHAiOjE2MTY5MjgwNzJ9.r8SSkwW-an2z-VonUboWKcX6-OaKy_MhY3e-lTGiqus`;
   }
 }
