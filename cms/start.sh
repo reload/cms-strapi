dd2688653722a8b05646bf4f70af1300e6fdbfbe
Mathias Piletti <mathiasp50@gmail.com>
2021-03-01 12:58:19 +0100

    Updated docs4

--- cms/start.sh
+++ cms/start.sh
@@ -2,7 +2,9 @@
 
 # Environment-specific configuration for Strapi.
 if [ "$PLATFORM_BRANCH" = main ]; then
+   echo "Starting in prod mode"
    yarn start
 else
+   echo "Starting in dev mode"
    yarn develop
 fi
